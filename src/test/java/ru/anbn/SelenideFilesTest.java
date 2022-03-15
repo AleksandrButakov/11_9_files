9443a06131d5eb248799f25873348f31711f03ec
Aleksandr Butakov <anbngm@gmail.com>
2022-03-15 09:38:08 +0500

    Init commit

---
+++ src/test/java/ru/anbn/SelenideFilesTest.java
@@ -0,0 +1,24 @@
+package ru.anbn;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideFilesTest {
+
+    @Test
+    void selenideDownloadTest() {
+        open("https://github.com/junit-team/junit5/blob/main/README.md");
+        try {
+            File downloadedFile = $("#raw-url").download();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
